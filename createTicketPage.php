179ebe2e5268819a5af8daaccce99fe850129ad8
charchalis <charchalis97@gmail.com>
2023-04-08 18:55:46 +0100

    css on createTicket page

--- createTicketPage.php
+++ createTicketPage.php
@@ -14,16 +14,18 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 <html>
 <head>
     <title>Ticket page</title>
+    <link href="css/style.css" rel="stylesheet"/>
 </head>
 <body>
+    <?php include('header.php')?>
     <h1>Ticket page</h1>
     <p>This is where tickets are created</p>
 
     <form action="database/createTicket.php" method="post">  
-	    <input type="text" name="title" placeholder="title">
+	    <input type="text" name="title" class="profileTextbox" placeholder="title">
         <p/>
-	    <input list="departments" name="department" placeholder="department">
-	    <input type="text" name="label" placeholder="label (ex: #feupRulzzz)">
+	    <input list="departments" name="department" class="profileTextbox" placeholder="department">
+	    <input type="text" name="label" class="profileTextbox" placeholder="label (ex: #feupRulzzz)">
         <datalist id="departments">
             <?php
                 foreach($_SESSION['departments'] as $department){
@@ -32,16 +34,14 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
             ?>
         </datalist>
         <p/>
-        <textarea name="text" rows="10" cols="50" placeholder="text"></textarea>
+        <textarea name="text" class="profileTextbox" rows="10" cols="50" placeholder="text"></textarea>
         <p/>
 	    <input type="submit" name="submit" value="submit">
     </form>
 
     <form action="home.php" method="post">
-	<input type="submit" name="submit" value="home">
+	    <input type="submit" name="submit" value="home">
     </form>
-
-
-    <a href="logout.php">Logout</a>
+    <?php include('footer.php')?>
 </body>
 </html
