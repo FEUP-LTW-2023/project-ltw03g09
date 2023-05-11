6c9426704283ce39b1cd9dc3e0de11b31b887ebe
charchalis <charchalis97@gmail.com>
2023-05-11 18:15:02 +0100

    css fixes

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 <div class="header">
-    <form action="home.php" method="post" class="profileButtonForm">
-	    <input style="background:none;border:none;color:#fff;" type="submit" name="submit" value="home"/>
-    </form>
+    <i style="color:#fff; display:flex; align-items: center; padding-left: 1em; font-size:20px;cursor:pointer" class="fas fa-home" onclick="window.location.href = 'home.php'"></i>
+
     <form action="profilePage.php" method="post" class="profileButtonForm">
 	    <input style="width:64px;height:64px;border-radius:50%;padding:1em;" src=<?php echo "'".$_SESSION['image']."'";?> type="image" name="submit" value="profile page"/>
     </form>
