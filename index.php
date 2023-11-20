f98b64c63c2bb2d670e7886d8a5d29fb289490ec
JoãoMartins <joaofmm2003@gmail.com>
2023-11-20 18:02:13 +0000

    fiz push num repositorio que n devia

--- index.php
+++ index.php
@@ -1,38 +1,33 @@
-<?php
-$produto = "Produto";
-$preco = "EUR 19.99";
-$descricao = "descrição aleatória";
-$envio = "Envio: Frete internacional econômico grátis.";
-$entrega = "Entrega: O tempo estimado é de 9 dias.";
-$retorno = "Devoluções: Devolução em até 30 dias. O comprador paga o frete de devolução.";
-?>
-
 <!DOCTYPE html>
 <html lang="en-US">
   <head>
-    <title>Trouble Tickets</title>
-    <link rel="stylesheet" href="css/estilo.css">
+    <title>Trouble Tickets</title>    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+    <link href="layout.css" rel="stylesheet">
+    <link href="responsive.css" rel="stylesheet">
+    <link href="comments.css" rel="stylesheet">
+    <link href="forms.css" rel="stylesheet">
   </head>
   <body>
-    <div class="header">
-      <div class="logo">Sky Buy</div>
-      <div class="barra-pesquisa">
-        <input type="text" placeholder="Pesquisar...">
-      </div>
-      <div class="buttons">
-        <button>Shop</button>
-        <button>Who we are</button>
-        <button>My profile</button>
-        <button>Shopping Card</button>
-      </div>
+    <h1>LOGIN</h1>
+    <div class="blurred-line"></div>
+    <div class="form-container">
+      <form action="database/authenticate.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" name="submit" value="Login">
+      </form>
     </div>
-    <div class="produto">
-      <h2><?php echo $produto; ?></h2>
-      <p class="preco"><?php echo $preco; ?></p>
-      <p><?php echo $descricao; ?></p>
-      <p><?php echo $envio; ?></p>
-      <p><?php echo $entrega; ?></p>
-      <p><?php echo $retorno; ?></p>
-    </div>
+    <a href="pages/registerPage.php">Register</a>
   </body>
 </html>
+
+
+
+<?php
+$invalidAuthentication = $_GET['invalid'];
+if($invalidAuthentication) echo '<p>' . 'invalid username or password' . '</p>';
+?>
